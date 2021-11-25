2023553b5e92a4e1073917d7be6ffe7794a2e437
Vagner Paulino <vagnerpaulinop@gmail.com>
2021-11-25 15:50:22 -0300

    Flutter Project Creation with Sqlite & Sqflite CRUD With Local Scheduled Notifications | GetX Listview

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_to_do_app/services/notification_services.dart';
+import 'package:flutter_to_do_app/services/theme_services.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var notifyHelper;
+  @override
+  void initState() {
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification();
+    notifyHelper.requestIOSPermissions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(),
+      body: Column(
+        children: [
+          Text(
+            "Theme Data",
+            style: TextStyle(fontSize: 30),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      leading: GestureDetector(
+        onTap: () {
+          ThemeService().switchTheme();
+          notifyHelper.displayNotification(
+              title: "Theme Changed",
+              body: Get.isDarkMode
+                  ? "Activated Light Theme"
+                  : "Activated Dark Theme");
+          notifyHelper.scheduledNotification();
+        },
+        child: Icon(
+          Icons.nightlight_round,
+          size: 20,
+        ),
+      ),
+      actions: [
+        Icon(
+          Icons.person,
+          size: 20,
+        ),
+        SizedBox(
+          width: 20,
+        ),
+      ],
+    );
+  }
+}
